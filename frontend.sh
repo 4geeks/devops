b1a56afc3953f1082f29d00942d4eeb8c3558060
Angel Lacret <alacret@4geeks.co>
2018-03-05 13:41:50 -0500

    FEATURE: frontend file: add yarn

--- frontend.sh
+++ frontend.sh
@@ -2,7 +2,10 @@
 
 
 curl -sL https://deb.nodesource.com/setup_8.x -o nodesource_setup.sh
-bash nodesource_setup.sh
-apt-get install nodejs build-essential git
-npm install pm2 -g
+sudo bash nodesource_setup.sh
+sudo apt-get install -y nodejs build-essential git
+sudo npm install pm2 -g
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt-get update && sudo apt-get install yarn
 
