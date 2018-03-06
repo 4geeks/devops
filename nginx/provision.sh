f02b9855bcc804789efb4c75f3fc5d288e580eff
Angel Lacret <alacret@4geeks.co>
2018-03-05 20:00:56 -0500

    FEATURE: add nginx provision

--- nginx/provision.sh
+++ nginx/provision.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 sudo apt-get install -y nginx
-mkdir ~/log
-touch ~/log/error.log
-touch ~/log/access.log
+mkdir ~/log/nginx
+touch ~/log/nginx/error.log
+touch ~/log/nginx/access.log
