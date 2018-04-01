7dafeb461911a2b988af1f9a28ded2a544b2be31
Angel Lacret <alacret@gmail.com>
2018-04-01 17:56:31 -0400

    nginx

--- nginx/provision.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-sudo apt-get install -y nginx
-mkdir ~/log/nginx
-touch ~/log/nginx/error.log
-touch ~/log/nginx/access.log
--- nginx/restart.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-
-sudo service nginx restart
