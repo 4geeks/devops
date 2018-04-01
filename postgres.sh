2af89743380e44ec04cc503a654a2b8211f37742
Angel Lacret <alacret@gmail.com>
2018-04-01 16:54:14 -0400

    postgres.sh

--- postgres.sh
+++ postgres.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 apt update
-apt install -y postgresql-10.3 postgresql-server-dev-10.2
+apt install -y postgresql-9.5 postgresql-server-dev-9.5
 sudo -u postgres bash -c "createuser --createdb --echo --login --superuser django "
-sudo -u postgres bash -c "psql -c \"ALTER ROLE vagrant WITH PASSWORD 'django';\""
-sudo -u postgres bash -c "psql -c \"ALTER ROLE vagrant WITH PASSWORD 'django';\""
+sudo -u postgres bash -c "psql -c \"ALTER ROLE django WITH PASSWORD 'django';\""
+sudo -u postgres bash -c "psql -c \"ALTER ROLE django WITH PASSWORD 'django';\""
