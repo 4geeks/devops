ca360ad379bb32fffeab1c18f3e8b0ab591f960e
Angel Lacret <alacret@gmail.com>
2018-04-01 17:10:06 -0400

    postgres.sh gunicorn.sh

--- postgres.sh
+++ postgres.sh
@@ -5,5 +5,6 @@ apt install -y postgresql-9.5 postgresql-server-dev-9.5
 sudo -u postgres bash -c "createuser --createdb --echo --login --superuser django "
 sudo -u postgres bash -c "psql -c \"ALTER ROLE django WITH PASSWORD 'django';\""
 sudo -u postgres bash -c "psql -c \"ALTER ROLE django WITH PASSWORD 'django';\""
+sudo -u postgres bash -c "psql -c \"CREATE DATABASE django OWNER django;\""
 sudo echo "local   all             django          md5" >> /etc/postgresql/9.5/main/pg_hba.conf
 sudo service postgresql restart
