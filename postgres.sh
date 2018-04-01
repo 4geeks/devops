4e6c0b137a810b5e99de5ca9a2aba8e124182935
Angel Lacret <alacret@gmail.com>
2018-04-01 17:03:27 -0400

    postgres.sh

--- postgres.sh
+++ postgres.sh
@@ -5,3 +5,5 @@ apt install -y postgresql-9.5 postgresql-server-dev-9.5
 sudo -u postgres bash -c "createuser --createdb --echo --login --superuser django "
 sudo -u postgres bash -c "psql -c \"ALTER ROLE django WITH PASSWORD 'django';\""
 sudo -u postgres bash -c "psql -c \"ALTER ROLE django WITH PASSWORD 'django';\""
+sudo echo "local   all             django          md5" >> /etc/postgresql/9.5/main/pg_hba.conf
+sudo service postgresql restart
